014487a073bb5650fe9773c21f3e79207b1230ce
Kalyan Chowdhury <74340429+Humblekalyan001@users.noreply.github.com>
2021-02-13 22:07:53 +0530

    Update Same Tree.cpp

--- LEETCODE Problems/Same Tree.cpp
+++ LEETCODE Problems/Same Tree.cpp
@@ -1,3 +1,4 @@
+//Recursive 
 class Solution 
 {
 public:
@@ -20,3 +21,27 @@ public:
         
     }
 };
+//iterative
+
+  class Solution {
+  public:
+      bool isSameTree(TreeNode* p, TreeNode* q) {
+          queue<TreeNode *> queue;
+          queue.push(p);
+          queue.push(q);
+          while (queue.size()!=0){
+              TreeNode * q2=queue.front();
+              queue.pop();
+              TreeNode * q1=queue.front();
+              queue.pop();
+              if (q1==nullptr && q2==nullptr) continue;
+              if (q1==nullptr || q2==nullptr) return false;
+              if (q1->val!=q2->val) return false;
+              queue.push(q1->left);
+              queue.push(q2->left);
+              queue.push(q1->right);
+              queue.push(q2->right);
+          }
+          return true;
+      }
+  };
